8b76512baa4a3ec3e988e89f8d08436fd619e5ec
strothj <jason@jasons.io>
2016-10-12 02:21:47 -0400

    Added Bartender with createDrink method

--- js/app.js
+++ js/app.js
@@ -83,13 +83,35 @@ var Pantry = function(ingredients) {
   this.ingredients = ingredients;
 };
 
+// Returns a random ingredient of the specified type.
+Pantry.prototype.randomIngredient = function(type) {
+  var ingredients = this.ingredients[type];
+  return ingredients[Math.floor(Math.random() * (ingredients.length))];
+};
+
+var Bartender = function(pantry) {
+  this.pantry = pantry;
+};
+
+// Returns a string composed of a comma separated list of drink ingredients.
+Bartender.prototype.createDrink = function(preferences) {
+  var ingredients = [];
+  for (var i = 0; i < preferences.length; i++) {
+    ingredients.push(this.pantry.randomIngredient(preferences[i]));
+  }
+  return ingredients.map(function(item) {
+    return item.name;
+  }).join(', ');
+};
+
 $(function() {
   var questions = createQuestions();
   var ingredients = createIngredients();
   var pantry = new Pantry(ingredients);
+  var bartender = new Bartender(pantry);
   var questionnaire = new Questionnaire($('#js-form'), questions);
   questionnaire.submit = function(preferences) {
-    console.log(preferences);
-    // TODO: Ask bartender to create drink here.
+    var drink = bartender.createDrink(preferences);
+    console.log(drink);
   };
 });
