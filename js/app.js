f21801aa6801619dc22a06b1ba484fcd4c38ba6b
strothj <jason@jasons.io>
2016-10-12 00:39:11 -0400

    Refactor and created preference object

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 'use strict';
 
+// Returns an object which represents a user's preference for an ingredient.
 var Question = function(question, ingredientType) {
-  this.question = question;
   this.ingredientType = ingredientType;
+
+  // Create a jQuery element using the Question template.
+  this.element = $(this.template
+    .replace(/\$\{question\}/g, question)
+    .replace(/\$\{id\}/g, 'js-answer-' + this.ingredientType)
+  );
+  // Store the element for the yes radio input so it can be checked later.
+  this.yesElement = this.element.find('#js-answer-' + this.ingredientType + '-yes');
 };
 
 Question.prototype.template = '<fieldset>' +
@@ -13,73 +21,62 @@ Question.prototype.template = '<fieldset>' +
   '<input id="${id}-no" type="radio" name="${id}">' +
 '</fieldset>';
 
-// Returns a jQuery element containing the question.
-Question.prototype.createElement = function() {
-  var renderedHTML = this.template
-    .replace(/\$\{question\}/g, this.question)
-    .replace(/\$\{id\}/g, 'js-answer-' + this.ingredientType);
-  var elem = $(renderedHTML);
-  this.yesElem = elem.find('#js-answer-' + this.ingredientType + '-yes');
-  return elem;
-};
-
 // Returns a boolean. Returns true if the user wants the ingredient type.
 Question.prototype.wants = function() {
-  return this.yesElem.prop('checked');
+  return this.yesElement.prop('checked');
 };
 
 // Returns an array of Questions.
 function createQuestions() {
   return [
-    [ 'Do ye like yer drinks strong?', 'strong' ],
-    [ 'Do ye like it with a salty tang?', 'salty' ],
-    [ 'Are ye a lubber who likes it bitter?', 'bitter' ],
-    [ 'Would ye like a bit of sweetness with yer poison?', 'sweet' ],
-    [ 'Are ye one for a fruity finish?', 'fruity' ]
-  ].map(function(question) {
-    return new Question(question[0] /* question */, question[1] /* ingredientType */);
-  });
+    new Question('Do ye like yer drinks strong?', 'strong'),
+    new Question('Do ye like it with a salty tang?', 'salty'),
+    new Question('Are ye a lubber who likes it bitter?', 'bitter'),
+    new Question('Would ye like a bit of sweetness with yer poison?', 'sweet'),
+    new Question('Are ye one for a fruity finish?', 'fruity')
+  ];
 }
 
 var Ingredient = function(name) {
   this.name = name;
 };
 
-// Returns an array of objects containing Ingredients grouped by type.
-function createIngredients() /* { type: string, ingredients: Ingredient[] }[] */{
-  return  [
-    { type: 'strong', ingredients: ['glug of rum', 'slug of whisky', 'splash of gin'] },
-    { type: 'salty', ingredients: ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'] },
-    { type: 'bitter', ingredients: ['shake of bitters', 'splash of tonic', 'twist of lemon peel'] },
-    { type: 'sweet', ingredients: ['sugar cube', 'spoonful of honey', 'splash of color'] },
-    { type: 'fruity', ingredients: ['slice of orange', 'dash of cassis', 'cherry on top'] }
-  ].map(function(ingredientGroup) {
-    return {
-      type: ingredientGroup.type,
-      ingredients: ingredientGroup.ingredients.map(function(ingredient) {
-        return new Ingredient(ingredient);
-      })
-    };
-  });
+// Returns an object containing Ingredients grouped by type.
+function createIngredients() {
+  var groupedIngredients = function(ingredients) {
+    return ingredients.map(function(item) {
+      return new Ingredient(item);
+    });
+  };
+
+  return {
+    strong: groupedIngredients(['glug of rum', 'slug of whisky', 'splash of gin']),
+    salty: groupedIngredients(['olive on a stick', 'salt-dusted rim', 'rasher of bacon']),
+    bitter: groupedIngredients(['shake of bitters', 'splash of tonic', 'twist of lemon peel']),
+    sweet: groupedIngredients(['sugar cube', 'spoonful of honey', 'splash of color']),
+    fruity: groupedIngredients(['slice of orange', 'dash of cassis', 'cherry on top'])
+  };
 }
 
-var Questionnaire = function(questions) {
-  this.questions = questions;
-};
-
-Questionnaire.prototype.render = function(formElement) {
-  for (var i = 0; i < this.questions.length; i++) {
-    formElement.append(this.questions[i].createElement());
+// Creates the user interface which asks for the user's drink preferences.
+var Questionnaire = function(formElement, questions) {
+  var submitElement = $('<buttom type="submit">Make my drink!</button>');
+  for (var i = 0; i < questions.length; i++) {
+    formElement.append(questions[i].element);
   }
-  var submitElem = $('<buttom type="submit">Make my drink!</button>');
-  var _this = this;
-  submitElem.click(function(e) {
-    e.preventDefault();
-    for (i = 0; i < _this.questions.length; i++) {
-      console.log(_this.questions[i], _this.questions[i].wants());
+  formElement.append(submitElement);
+
+  var _this = this; // capture this so it can be used in the jQuery handler below.
+  submitElement.click(function(event) {
+    event.preventDefault();
+    var preferences = [];
+    for (i = 0; i < questions.length; i++) {
+      if (questions[i].wants()) {
+        preferences.push(questions[i].ingredientType);
+      }
     }
+    _this.submit(preferences);
   });
-  formElement.append(submitElem);
 };
 
 var Pantry = function(ingredients) {
@@ -90,6 +87,9 @@ $(function() {
   var questions = createQuestions();
   var ingredients = createIngredients();
   var pantry = new Pantry(ingredients);
-  var questionnaire = new Questionnaire(questions);
-  questionnaire.render($('#js-form'));
+  var questionnaire = new Questionnaire($('#js-form'), questions);
+  questionnaire.submit = function(preferences) {
+    console.log(preferences);
+    // TODO: Ask bartender to create drink here.
+  };
 });
