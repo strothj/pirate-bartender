c6b39d817807ee21d9a64f17cfb169b4f9afcb47
strothj <jason@jasons.io>
2016-10-17 04:27:54 -0400

    Added UglifyJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 var path = require('path');
 
+var webpack = require('webpack');
+
 var packageData = require('./package.json');
 
+var minify = process.argv.indexOf('--minify') != -1;
+
 var filename = [packageData.name, packageData.version, 'js'];
+var plugins = [];
+
+if (minify) {
+    filename.splice(filename.length - 1, 0, 'min');
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
 
 module.exports = {
   entry: path.resolve(__dirname, packageData.main),
@@ -22,5 +32,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: plugins
 };
